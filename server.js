7b2b75f4d7678458b102edb20d247249b96d18c2
Samad <samadislam186@gmail.com>
2021-01-03 12:14:11 +0530

    Server starting script added

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const ejs = require('ejs')
+const token = require('json-web-token')
+const app = express()
+
+const port = const.ENV.PORT || 3000
+
+app.get('\', (req, res) => {
+	res.render('index.ejs')
+});
+
+const server = app.listen((port) => {
+	if(!port){
+	console.out(`server is not running on port {$port}`);
+	}
+	else{
+		console.out(`server started`);
+	}
+}
